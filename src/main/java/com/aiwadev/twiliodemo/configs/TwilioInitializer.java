7ebb2a134049e733e59e7ac8e5f85f0b6f79a0c0
leonel47 <waboleonel@gmail.com>
2020-09-18 00:34:37 -0500

    TwilioInitializer class definition

---
+++ src/main/java/com/aiwadev/twiliodemo/configs/TwilioInitializer.java
@@ -0,0 +1,25 @@
+package com.aiwadev.twiliodemo.configs;
+
+import com.twilio.Twilio;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class TwilioInitializer {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(TwilioInitializer.class);
+
+    private final TwilioConfiguration twilioConfiguration;
+
+    @Autowired
+    public TwilioInitializer(TwilioConfiguration configuration) {
+        this.twilioConfiguration = configuration;
+        Twilio.init(
+                twilioConfiguration.getAccountSid(),
+                twilioConfiguration.getAuthToken()
+        );
+        LOGGER.info("Twilio initialization with ID {}", twilioConfiguration.getAccountSid());
+    }
+}
